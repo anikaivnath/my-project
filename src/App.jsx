21ad8d2abc8d1f3fccdd3c0f76ab6bde6d2718fd
anikaivnath <anikaivnath8@gmail.com>
2024-09-11 22:21:25 +0600

    Add name ,email,phone,github

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React from 'react';
 
-const BioData = () => {
+const BioData = (props) => {
   return (
-    
-    <div className='Name'>BioData</div>
-  )
-}
+    <div className='Bio-Data'>
+      <div>BioData</div>
+      
+      <div className='Personal Info'>
+        <h2>BioData of {props.name}</h2>
+      </div>
+      <div>
+      <p><strong>Email:</strong>{props.email}</p>
+      <p>
+        <strong>Phone:{props.pnum}</strong>
+      </p>
+      <p>
+        <strong>Github:</strong>{props.github}
+      </p>
 
-export default BioData
+      </div>
+    </div>
+  );
+};
+
+export default BioData;
